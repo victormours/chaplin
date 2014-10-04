af8bdb2ae4c99619c1ad9390b3915b349e838288
Victor Mours <victor.mours@gmail.com>
2014-10-04 19:52:01 +0200

    Fix all specs

--- spec/integration/server_spec.rb
+++ spec/integration/server_spec.rb
@@ -14,15 +14,14 @@ end
 
 describe 'Routes without API requests' do
   it 'serves the template' do
-    response = Net::HTTP.get(URI('http://localhost:8081/about'))
-    expect(response).to eq "Hello Chaplin!\n"
+    response = Net::HTTP.get(URI('http://localhost:8081/no_requests'))
+    expect(response).to eq "This is a static template.\n"
   end
 end
 
 describe 'Routes with get API requests' do
   it 'serves the template' do
     response = Net::HTTP.get(URI('http://localhost:8081/profile'))
-    expect(response).to eq "Hello Chaplin!\n"
+    expect(response).to eq "You name is Bob\n"
   end
-
 end
--- spec/integration_spec_helper.rb
+++ spec/integration_spec_helper.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 RSpec.configure do |config|
   config.before(:suite) do
     @api_pid = fork do
-      fake_api = Rack::File.new('spec/fixtures')
+      fake_api = Rack::File.new('spec/fixtures/fake_api')
       Rack::Server.start(app: fake_api, Port: 8080)
     end
     @chaplin_pid = fork do
-      Dir.chdir("spec/fake_chaplin_project")
+      Dir.chdir("spec/fixtures/fake_chaplin_project")
       chaplin_server = Chaplin::Server.new('localhost:8080')
 
       Rack::Server.start(app: chaplin_server, Port: 8081)
