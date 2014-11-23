e02385e5e5b9423fde6913625bb0acfb7201542b
Victor Mours <victor.mours@gmail.com>
2014-11-23 11:48:45 +0100

    Add project path to pages generator

--- lib/chaplin/pages.rb
+++ lib/chaplin/pages.rb
@@ -3,10 +3,10 @@ require_relative 'api_endpoint'
 
 class Chaplin
 
-  Pages = Struct.new(:pages_data, :layout_name) do
+  Pages = Struct.new(:pages_data, :project_path, :layout_name) do
 
-    def self.load(pages_data, layout_name = nil)
-      new(pages_data, layout_name).tap do |pages|
+    def self.load(pages_data, project_path, layout_name = nil)
+      new(pages_data, project_path, layout_name).tap do |pages|
         pages.load
       end
     end
@@ -15,17 +15,28 @@ class Chaplin
       @pages = {}
 
       pages_data.each do |template_name, raw_data_hash|
-        page =  Page.new(template_name, data_hash(raw_data_hash))
-        page.embed_in_layout(layout_name) if layout_name
-        @pages[template_name] = page
+        @pages[template_name] = build_page(template_name, raw_data_hash)
       end
     end
 
     def [](page_name)
-      @pages[page_name]
+      @pages[page_name] || build_page(page_name, {})
     end
 
     private
+
+    def build_page(template_name, raw_data_hash)
+      Page.new(
+        template_path(template_name),
+        data_hash(raw_data_hash)
+      ).tap do |page|
+        page.embed_in_layout(layout_name) if layout_name
+      end
+    end
+
+    def template_path(template_name)
+      project_path + '/templates/' + template_name
+    end
 
     def data_hash(raw_data_hash)
       raw_data_hash.each_with_object({}) do |(key, raw_api_endpoint), data_hash|
