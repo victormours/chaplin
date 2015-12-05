99f9af6b3a6608e6c1caa01844a645797147eb07
Victor Mours <victor.mours@gmail.com>
2015-12-05 19:43:31 +0100

    Make New silent when called from ruby code

--- lib/chaplin/new.rb
+++ lib/chaplin/new.rb
@@ -1,4 +1,3 @@
-
 class Chaplin
   module New
 
@@ -7,9 +6,6 @@ class Chaplin
       FileUtils.cp_r("#{__dir__}/../../sample_project/templates", new_project_path)
       FileUtils.cp("#{__dir__}/../../sample_project/app.yml", new_project_path)
       FileUtils.cp("#{__dir__}/../../sample_project/chaplin_config.yml", new_project_path)
-
-      puts "New chaplin project created in #{new_project_path}"
-      puts "Start the server with 'chaplin #{new_project_path}'"
     end
 
   end
