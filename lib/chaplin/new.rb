0a08d209010c00b13708b9c05fc6f47cd696653c
Victor Mours <victor.mours@gmail.com>
2015-12-07 11:41:47 +0100

    Stop using chaplin_config in app generator

--- lib/chaplin/new.rb
+++ lib/chaplin/new.rb
@@ -5,7 +5,6 @@ class Chaplin
       FileUtils.mkdir(new_project_path)
       FileUtils.cp_r("#{__dir__}/../../sample_project/templates", new_project_path)
       FileUtils.cp("#{__dir__}/../../sample_project/app.yml", new_project_path)
-      FileUtils.cp("#{__dir__}/../../sample_project/chaplin_config.yml", new_project_path)
     end
 
   end
